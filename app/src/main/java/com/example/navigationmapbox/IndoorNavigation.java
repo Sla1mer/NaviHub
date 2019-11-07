4b6061f9a46af0e30606538681bab89afac6f2c4
Назар Садик <Slameryt4@gmail.com>
2019-11-07 20:52:01 +0200

    Correct

--- app/src/main/java/com/example/navigationmapbox/IndoorNavigation.java
+++ app/src/main/java/com/example/navigationmapbox/IndoorNavigation.java
@@ -1,20 +1,43 @@
 package com.example.navigationmapbox;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class IndoorNavigation extends AppCompatActivity {
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import java.util.List;
+import android.widget.Toast;
+
+// classes needed to initialize map
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+
+
+public class IndoorNavigation extends AppCompatActivity implements OnMapReadyCallback, MapboxMap.OnMapClickListener, PermissionsListener {
 
     boolean flag = true;
+    private MapView mapView;
+    private MapboxMap mapboxMap;
 
-    @Override
+            @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Mapbox.getInstance(this, getString(R.string.access_token));
         setContentView(R.layout.activity_indoor);
+        mapView = findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
 
         final ImageView imageButton1 = (ImageView) findViewById(R.id.Floor1);
         final ImageView imageButton2 = (ImageView) findViewById(R.id.Floor2);
@@ -79,5 +102,79 @@ public class IndoorNavigation extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onMapReady(@NonNull final MapboxMap mapboxMap) {
+                this.mapboxMap = mapboxMap;
+                mapboxMap.setStyle(getString(R.string.navigation_guidance_day), new Style.OnStyleLoaded() {
+                    @Override
+                    public void onStyleLoaded(@NonNull Style style) {
+
+                    }
+                });
+    }
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+
+    }
+
+    @Override
+    public void onPermissionResult(boolean granted) {
+
+    }
+
+    @Override
+    public boolean onMapClick(@NonNull LatLng point) {
+        return false;
+    }
+
+    public void onMyButtonClick(View view)
+    {
+        Intent intent = new Intent(IndoorNavigation.this, MainActivity.class);
+        startActivity(intent);
     }
 }
